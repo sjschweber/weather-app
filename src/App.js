91576b733db3f958df9dafb554a7529b76d32645
sjschweber <schweber97@gmail.com>
2021-03-08 17:34:48 -0500

    Creates useEffect to make API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './WeatherCard.js';
+import axios from 'axios';
+
+const API_KEY = "NAQgnnEuhIjNziOdkNHGv4ZWBxrdUSqn";
 
 function App() {
+
+  const [location, setLocation] = useState("");
+
+  const x = "Toronto";
+
+  useEffect(() => {
+
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${location}`)
+      .then(res => {
+         console.log(res.data[0].Key);
+         return res.data[0].Key
+      })
+      .then(res =>{
+
+        axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${res}?apikey=${API_KEY}`)
+          .then(data =>{
+            console.log(data);
+            return data;
+          })
+      }
+
+      )
+
+  })
+
+  function onClick(e){
+    e.preventDefault();
+    setLocation(document.getElementById("locationText").value);
+    console.log(location);
+  }
 
   var days = [
     {
